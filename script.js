726d336515145350656fbafe66dd208cd54baf41
10051556 <43875433+10051556@users.noreply.github.com>
2021-09-09 18:03:34 +0800

    Revert "Revert "bug""
    This reverts commit 8a149cd58f729a4f2c2140457513c79ce6889ccf.

--- script.js
+++ script.js
@@ -3,31 +3,41 @@ const express = require('express'),
   fs = require('fs'),
   shell = require('shelljs'),
 
-   // Modify the folder path in which responses need to be stored
+  // Modify the folder path in which responses need to be stored
   folderPath = './Responses/',
   defaultFileExtension = 'json', // Change the default file extension
   bodyParser = require('body-parser'),
-  DEFAULT_MODE = 'writeFile',
+  DEFAULT_MODE = 'appendFile',
   path = require('path');
 
 // Create the folder path in case it doesn't exist
 shell.mkdir('-p', folderPath);
 
- // Change the limits according to your response size
-app.use(bodyParser.json({limit: '50mb', extended: true}));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); 
+// Change the limits according to your response size
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', (req, res) => res.send('Hello, I write data to file. Send them requests!'));
 
-app.post('/write', (req, res) => {
-  let extension = req.body.fileExtension || defaultFileExtension,
-    fsMode = req.body.mode || DEFAULT_MODE,
-    uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
-    filename = `${req.body.requestName}${uniqueIdentifier || ''}`,
-    filePath = `${path.join(folderPath, filename)}.${extension}`,
-    options = req.body.options || undefined;
 
-  fs[fsMode](filePath, req.body.responseData, options, (err) => {
+app.post('/write', (req, res) => {
+  let extension = req.body.fileExtension || defaultFileExtension;
+  let fsMode = DEFAULT_MODE;
+  let uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.niqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false;
+  let filename = `output`;
+  // filename = `${req.body.requestName}${uniqueIdentifier || ''}`;
+  let dataCount = Object.values(res).length;
+  console.log(dataCount);
+
+  let fileHead = dataCount < 1 ? "[" : "";
+  let fileTail = dataCount == 0 ? "]" : ",";
+  let fileData = "";
+  fileData += req.body.responseData;
+  let filePath = `${path.join(folderPath, filename)}.${extension}`;
+
+
+
+  fs[fsMode](filePath, fileHead + fileData + fileTail, (err) => {
     if (err) {
       console.log(err);
       res.send('Error');
@@ -36,6 +46,7 @@ app.post('/write', (req, res) => {
       res.send('Success');
     }
   });
+
 });
 
 app.listen(3000, () => {
