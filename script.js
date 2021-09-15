23d0654d7614079480120d28b3ddf0ac7fd42cbf
10051556 <43875433+10051556@users.noreply.github.com>
2021-09-15 17:59:55 +0800

    ,

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const { readFile } = require('fs/promises');
 
 const express = require('express'),
-  app = express(),
-  fs = require('fs'),
-  shell = require('shelljs');;
+    app = express(),
+    fs = require('fs'),
+    shell = require('shelljs'),
+    $ = require('jquery')
 
 // Modify the folder path in which responses need to be stored
-folderPath = './Responses/',
-  defaultFileExtension = 'json', // Change the default file extension
-  bodyParser = require('body-parser'),
-  DEFAULT_MODE = 'appendFile',
-  path = require('path');
+path = require('path'),
+    folderPath = './Responses/',
+    defaultFileExtension = 'json', // Change the default file extension
+    bodyParser = require('body-parser'),
+    DEFAULT_MODE = 'appendFile';
 
 // Create the folder path in case it doesn't exist
 shell.mkdir('-p', folderPath);
@@ -21,35 +22,48 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', (req, res) => res.send('Hello, I write data to file. Send them requests!'));
 
-let fileData = "["
 app.post('/write', (req, res) => {
-  console.log("running")
-  let extension = req.body.fileExtension || defaultFileExtension,
-    fsMode = req.body.mode || DEFAULT_MODE,
-    uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
-    filename = `output`,
-    // filename = `${req.body.requestName}${uniqueIdentifier || ''}`,
-    filePath = `${path.join(folderPath, filename)}.${extension}`,
-    options = req.body.options || undefined;
-  fileData += req.body.responseData;
-  fileData += ","
-  // file.push(fileData)
+    console.log("running")
+    let extension = req.body.fileExtension || defaultFileExtension,
+        fsMode = req.body.mode || DEFAULT_MODE,
+        uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
+        filename = `output`,
+        // filename = `${req.body.requestName}${uniqueIdentifier || ''}`,
+        filePath = `${path.join(folderPath, filename)}.${extension}`,
+        options = req.body.options || undefined;
 
-  fs.appendFile("./Responses\/output.json", fileData, (err) => {
-    if (err) {
-      console.log(err);
-      res.send('Error');
-    }
-    else {
-      res.send('Success');
-    }
-  });
+    console.log(req.body.responseCode);
+    // fileData = req.responseCode;
 
+    // fs.appendFile(filePath, fileData, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //         res.send('Error');
+    //     } else {
+    //         res.send('Success');
+    //     }
+    // });
+
+    $('#myTable').DataTable({
+        // data: dataSet,
+        fixedHeader: true,
+
+        columns: [
+            { data: responseName },
+            { data: responseCode },
+            // { data: "allTests.0.Validate schema" },
+            // { data: "allTests.0.Response time is less than 10000ms" },
+            // { data: "allTests.0.Status code is 200" },
+            // { data: "allTests.0.Error is True" },
+            // { data: "allTests.0.Code is Correct" },
+        ]
+    });
 })
 
 
+
 app.listen(3000, () => {
-  console.log('ResponsesToFile App is listening now! Send them requests my way!');
-  console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
+    console.log('ResponsesToFile App is listening now! Send them requests my way!');
+    console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
 });
 
